7b1dce4d7d30974da0b0b9ab42d1ff4e52673841
Nga QUAN <quan.nga@deq.state.or.us>
2019-11-05 08:34:24 -0800

    added msd - ef razor page tutorial 1

---
+++ msd/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using msd.Data;
+using msd.Domain.Models;
+
+namespace msd.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly msd.Data.SchoolContext _context;
+
+        public DetailsModel(msd.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ msd/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using msd.Data;
+using msd.Domain.Models;
+
+namespace msd.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly msd.Data.SchoolContext _context;
+
+        public IndexModel(msd.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Students.ToListAsync();
+        }
+    }
+}
